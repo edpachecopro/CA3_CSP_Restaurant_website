4ef290e809cd93d74e0c9ef4f637e383f067df07
Edgard Pacheco <72029558+edpachecopro@users.noreply.github.com>
2021-05-12 04:06:37 +0100

    CSS and HTML improvement #02
    - Menu section with bootstrap 5
    - CSS adjustment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+
+
+
+
+
 // Script created to show the user from ramdom.me
-
-
     //Get API
     fetch('https://randomuser.me/api/?results=8')
         .then(res => res.json())
@@ -16,46 +19,115 @@
             author.forEach(function (lists) {
                 output += `
   
-                
-            <img src="${lists.picture.large}" alt="${lists.name.first}">
+            <div class="block-user"> 
 
-            <table class=" card table table-striped" >
-                <tr>
-                  <th>DOB</th>
-                  <td> ${lists.dob.date}</td>
-                </tr>
-                <tr>
-                  <th>AGE:</th>
-                  <td>${lists.dob.age}</td>
-                </tr>
-                <tr>
-                  <th>EMAIL:</th>
-                  <td> ${lists.email} </td>
-                </tr>
-                <tr>
-                    <th>GENDER:</th>
-                    <td> ${lists.gender}</td>
-                  </tr>
-                  <tr>
-                    <th>CITY:</th>
-                    <td> ${lists.location.city} </td>
-                  </tr>
-                  <tr>
-                    <th>COUNTRY:</th>
-                    <td> ${lists.location.country}</td>
-                  </tr>
-                  <tr>
-                    <th>POSTCODE:</th>
-                    <td> ${lists.location.postcode} </td>
-                  </tr>
-              </table>
-
+                    <div class="round">    
+                    <img src="${lists.picture.large}" alt="${lists.name.first}">
+                    </div>
+                    <div class="data-users">
+                        <div class="name"> ${lists.name.first}</div>
+                        
+                            <table class="table table-striped tb-mg" >
+                                <tr>
+                                <th>DOB</th>
+                                <td> ${lists.dob.date}</td>
+                                </tr>
+                                <tr>
+                                <th>AGE:</th>
+                                <td>${lists.dob.age}</td>
+                                </tr>
+                                <tr>
+                                <th>EMAIL:</th>
+                                <td> ${lists.email} </td>
+                                </tr>
+                                <tr>
+                                    <th>GENDER:</th>
+                                    <td> ${lists.gender}</td>
+                                </tr>
+                                <tr>
+                                    <th>CITY:</th>
+                                    <td> ${lists.location.city} </td>
+                                </tr>
+                                <tr>
+                                    <th>COUNTRY:</th>
+                                    <td> ${lists.location.country}</td>
+                                </tr>
+                                <tr>
+                                    <th>POSTCODE:</th>
+                                    <td> ${lists.location.postcode} </td>
+                                </tr>
+                            </table>
+                        </div>    
+              </div>
 
                 `;
             });
 
+            var str = "Hello world, %ˆ&&**(()) welcome to the universe.";
+            var n = str.includes("$");
 
             //Show On Our Screen All Data
             document.getElementById('users').innerHTML = output;
 
         });
+
+
+
+
+        // PSWD Validator
+
+function validator(pswd){
+    return document.getElementById(pswd);
+  }
+  function show_pswd(pswd){
+    return document.getElementsByClassName(pswd);
+  }
+
+//function to show the password
+  show_pswd("show-password")[0].addEventListener("click",function(){
+    show_pswd("show-password")[0].classList.toggle("active");
+    if(validator("password-field").getAttribute("type") == "password"){
+      validator("password-field").setAttribute("type","text");
+    } else {
+      validator("password-field").setAttribute("type","password");
+    }
+  });
+  
+  validator("password-field").addEventListener("focus",function(){
+    show_pswd("password-policies")[0].classList.add("active");
+  });
+  validator("password-field").addEventListener("blur",function(){
+    show_pswd("password-policies")[0].classList.remove("active");
+  });
+  
+  validator("password-field").addEventListener("keyup",function(){
+    let password = validator("password-field").value;
+    
+    if(/[A-Z]/.test(password)){
+        show_pswd("policy-uppercase")[0].classList.add("active");
+    } else {
+        show_pswd("policy-uppercase")[0].classList.remove("active");
+    }
+    
+    if(/[0-9]/.test(password)){
+        show_pswd("policy-number")[0].classList.add("active");
+    } else {
+        show_pswd("policy-number")[0].classList.remove("active");
+    }
+    
+    if(/[^A-Za-z0-9]/.test(password)){
+        show_pswd("policy-special")[0].classList.add("active");
+    } else {
+        show_pswd("policy-special")[0].classList.remove("active");
+    }
+    
+    if(password.length > 7){
+        show_pswd("policy-length")[0].classList.add("active");
+    } else {
+        show_pswd("policy-length")[0].classList.remove("active");
+    }
+  });
+
+
+
+
